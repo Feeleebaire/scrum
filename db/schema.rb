f48d80ca6b95d03c96e2259b0bb59b396ac64a3e
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-17 20:58:30 +0100

    model sprint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317194206) do
+ActiveRecord::Schema.define(version: 20170317195758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170317194206) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["backlog_id"], name: "index_products_on_backlog_id", using: :btree
+  end
+
+  create_table "sprints", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
